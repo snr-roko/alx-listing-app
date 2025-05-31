0eaba6a5cd5f22e9a1aec8854fd125ccb0586604
snr-roko <rbbagyei@outlook.com>
2025-05-31 15:28:18 +0000

    Refactor: Implement Header and Footer components; update Layout and Home page structure

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer : React.FC = () => {
+  return (
+    <footer>
+      <div></div>
+      <div></div>
+      <div>
+        <p>Some hotel requires you to cancel more than 24 hours before check-in. Details here</p>
+        <div>
+          <a href="#">Terms of Service</a>
+          <a href="#">Policy service</a>
+          <a href="#">Cookies Policy</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/layout/Header.tsx
@@ -0,0 +1,21 @@
+const Header : React.FC = () => {
+  return (
+    <header>
+      <div> 
+        <div></div>
+        <div>
+          <p>Overseas trip? Get the latest information on travel guides</p>
+        </div>
+        <div>
+          <button type="button">
+            More Info
+          </button>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
